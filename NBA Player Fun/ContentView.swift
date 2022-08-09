12aac56394ab7fd2b1f9217f1c7adab2483252be
burakadam <buraksahinerden@gmail.com>
2022-08-09 13:34:37 +0300

    Initial Commit

---
+++ NBA Player Fun/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NBA Player Fun
+//
+//  Created by Burak Şahin Erden on 9.08.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NBA Player Fun/NBA_Player_FunApp.swift
@@ -0,0 +1,17 @@
+//
+//  NBA_Player_FunApp.swift
+//  NBA Player Fun
+//
+//  Created by Burak Şahin Erden on 9.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct NBA_Player_FunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
